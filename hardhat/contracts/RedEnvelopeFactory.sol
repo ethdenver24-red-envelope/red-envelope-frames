95e8cd9643b34de3040ffa7784a4d22a7551f276
webthethird <ratzbodell@gmail.com>
2024-02-29 16:34:25 -0700

    `numEnvelopes`

--- hardhat/contracts/RedEnvelopeFactory.sol
+++ hardhat/contracts/RedEnvelopeFactory.sol
@@ -8,12 +8,14 @@ import "./RedEnvelope.sol";
 import "./interfaces/IRedEnvelopeFactory.sol";
 
 contract RedEnvelopeFactory is IRedEnvelopeFactory {
+    uint256 public numEnvelopes;
     RedEnvelope[] public envelopes;
 
     function create(address token, uint256 maxGifts, euint32 totalAmount) external returns (IRedEnvelope) {
         EncryptedERC20 erc20 = EncryptedERC20(token);
         RedEnvelope envelope = new RedEnvelope(msg.sender, erc20, maxGifts, totalAmount);
         envelopes.push(envelope);
+        numEnvelopes += 1;
         erc20.transferFrom(msg.sender, address(envelope), totalAmount);
         emit RedEnvelopeCreated(envelope, msg.sender, token, maxGifts);
         return envelope;
