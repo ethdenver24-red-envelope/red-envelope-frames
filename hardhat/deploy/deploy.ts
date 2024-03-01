eb8627c04806bfa2079da1aa5cdce8285f10067d
webthethird <ratzbodell@gmail.com>
2024-02-29 17:47:38 -0700

    Update deploy script

--- hardhat/deploy/deploy.ts
+++ hardhat/deploy/deploy.ts
@@ -5,14 +5,22 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployed = await deploy("EncryptedERC20", {
+  const deployedERC20 = await deploy("EncryptedERC20", {
     from: deployer,
     args: [],
     log: true,
   });
 
-  console.log(`EncryptedERC20 contract: `, deployed.address);
+  console.log(`EncryptedERC20 contract: `, deployedERC20.address);
+
+  const deployedFactory = await deploy("RedEnvelopeFactory", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
+
+  console.log(`RedEnvelopeFactory contract: `, deployedFactory.address);
 };
 export default func;
 func.id = "deploy_encryptedERC20"; // id required to prevent reexecution
-func.tags = ["EncryptedERC20"];
+func.tags = ["EncryptedERC20", "RedEnvelopeFactory"];
