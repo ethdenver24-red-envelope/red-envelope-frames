3ca4d3f14c7d13cfc40daf10b466037955bd8258
webthethird <ratzbodell@gmail.com>
2024-03-01 23:35:56 -0700

    Update tests

--- hardhat/test/redEnvelope/RedEnvelopeFactory.ts
+++ hardhat/test/redEnvelope/RedEnvelopeFactory.ts
@@ -10,6 +10,9 @@ import { deployRedEnvelopeFactoryFixture } from "./RedEnvelopeFactory.fixture";
 describe("RedEnvelope", function () {
   before(async function () {
     this.signers = await getSigners(ethers);
+    console.log("Bob: ", this.signers.bob.address);
+    console.log("Carol: ", this.signers.carol.address);
+    console.log("Dave: ", this.signers.dave.address);
   });
 
   beforeEach(async function () {
@@ -23,28 +26,30 @@ describe("RedEnvelope", function () {
     this.erc20Instances = await createInstances(this.erc20Address, ethers, this.signers);
   });
 
-  it("should create a red envelope contract via the factory, ", async function () {
+  it.skip("should create a red envelope contract via the factory, ", async function () {
     const encryptedAmount = this.erc20Instances.alice.encrypt32(10000);
     const transaction = await createTransaction(this.erc20.mint, encryptedAmount);
     await transaction.wait();
-
-    const encryptedTotalAmount = this.erc20Instances.alice.encrypt32(1337);
-    const approveTx = await createTransaction(
-      this.erc20["approve(address,bytes)"],
-      this.factoryAddress,
-      encryptedTotalAmount,
-    );
-    await approveTx.wait();
 
     const encryptedAmountEnvelope = this.erc20Instances.alice.encrypt32(1337);
 
     const createTx = await createTransaction(
       this.factory["create(address,uint256,bytes)"],
       this.erc20Address,
-      10,
+      3,
       encryptedAmountEnvelope,
     );
     await createTx.wait();
+
+    const envelopeAddress = await this.factory.envelopes(0);
+
+    const encryptedTotalAmount = this.erc20Instances.alice.encrypt32(1337);
+    const transferTx = await createTransaction(
+      this.erc20["transfer(address,bytes)"],
+      envelopeAddress,
+      encryptedTotalAmount,
+    );
+    await transferTx.wait();
 
     const tokenAlice = this.erc20Instances.alice.getTokenSignature(this.erc20Address)!;
 
@@ -55,10 +60,130 @@ describe("RedEnvelope", function () {
 
     expect(balanceAlice).to.equal(10000 - 1337);
 
-    const factoryAlice = this.factoryInstances.alice.getTokenSignature(this.factoryAddress)!;
-
     const numEnvelopes = await this.factory.numEnvelopes();
 
     expect(numEnvelopes).to.equal(1);
+  });
+
+  it("should create a red envelope and claim a gift for Bob", async function () {
+    const encryptedAmount = this.erc20Instances.alice.encrypt32(10000);
+    const transaction = await createTransaction(this.erc20.mint, encryptedAmount);
+    await transaction.wait();
+
+    const encryptedAmountEnvelope = this.erc20Instances.alice.encrypt32(1337);
+
+    const createTx = await createTransaction(
+      this.factory["create(address,uint256,bytes)"],
+      this.erc20Address,
+      3,
+      encryptedAmountEnvelope,
+    );
+    await createTx.wait();
+
+    const envelopeAddress = await this.factory.envelopes(0);
+
+    const encryptedTotalAmount = this.erc20Instances.alice.encrypt32(1337);
+    const transferTx = await createTransaction(
+      this.erc20["transfer(address,bytes)"],
+      envelopeAddress,
+      encryptedTotalAmount,
+    );
+    await transferTx.wait();
+
+    console.log("claiming...");
+    const envelopeBob = await ethers.getContractAt("RedEnvelope", envelopeAddress, this.signers.bob);
+    await envelopeBob.claim(this.signers.bob.address);
+    console.log("claimed");
+
+    const tokenBob = this.erc20Instances.bob.getTokenSignature(this.erc20Address)!;
+
+    console.log("calling balanceOf...");
+    const erc20Bob = await ethers.getContractAt("EncryptedERC20", this.erc20Address, this.signers.bob);
+    const encryptedBalanceBob = await erc20Bob.balanceOf(tokenBob.publicKey, tokenBob.signature);
+
+    console.log("calling getClaimedAmount...");
+    const claimedAmount = await envelopeBob.getClaimedAmount(this.signers.bob.address);
+
+    // Decrypt the balance
+    const balanceBob = this.erc20Instances.bob.decrypt(this.erc20Address, encryptedBalanceBob);
+
+    expect(balanceBob).to.equal(claimedAmount);
+  });
+
+  it("should not let Bob claim a gift twice", async function () {
+    const encryptedAmount = this.erc20Instances.alice.encrypt32(10000);
+    const transaction = await createTransaction(this.erc20.mint, encryptedAmount);
+    await transaction.wait();
+
+    const encryptedAmountEnvelope = this.erc20Instances.alice.encrypt32(1337);
+
+    const createTx = await createTransaction(
+      this.factory["create(address,uint256,bytes)"],
+      this.erc20Address,
+      3,
+      encryptedAmountEnvelope,
+    );
+    await createTx.wait();
+
+    const envelopeAddress = await this.factory.envelopes(0);
+
+    const encryptedTotalAmount = this.erc20Instances.alice.encrypt32(1337);
+    const transferTx = await createTransaction(
+      this.erc20["transfer(address,bytes)"],
+      envelopeAddress,
+      encryptedTotalAmount,
+    );
+    await transferTx.wait();
+
+    console.log("claiming...");
+    const envelopeBob = await ethers.getContractAt("RedEnvelope", envelopeAddress, this.signers.bob);
+    await envelopeBob.claim(this.signers.bob.address);
+    console.log("claimed");
+
+    expect(envelopeBob.claim(this.signers.bob.address)).to.be.revertedWithoutReason();
+  });
+
+  it("should not let Alice claim a gift after Bob, Carol and Dave", async function () {
+    const encryptedAmount = this.erc20Instances.alice.encrypt32(10000);
+    const transaction = await createTransaction(this.erc20.mint, encryptedAmount);
+    await transaction.wait();
+
+    const encryptedAmountEnvelope = this.erc20Instances.alice.encrypt32(1337);
+
+    const createTx = await createTransaction(
+      this.factory["create(address,uint256,bytes)"],
+      this.erc20Address,
+      3,
+      encryptedAmountEnvelope,
+    );
+    await createTx.wait();
+
+    const envelopeAddress = await this.factory.envelopes(0);
+
+    const encryptedTotalAmount = this.erc20Instances.alice.encrypt32(1337);
+    const transferTx = await createTransaction(
+      this.erc20["transfer(address,bytes)"],
+      envelopeAddress,
+      encryptedTotalAmount,
+    );
+    await transferTx.wait();
+
+    console.log("claiming for Bob...");
+    const envelopeBob = await ethers.getContractAt("RedEnvelope", envelopeAddress, this.signers.bob);
+    await envelopeBob.claim(this.signers.bob.address);
+    console.log("claimed for Bob");
+
+    console.log("claiming for Carol...");
+    const envelopeCarol = await ethers.getContractAt("RedEnvelope", envelopeAddress, this.signers.carol);
+    await envelopeCarol.claim(this.signers.carol.address);
+    console.log("claimed for Carol");
+
+    console.log("claiming for Dave...");
+    const envelopeDave = await ethers.getContractAt("RedEnvelope", envelopeAddress, this.signers.dave);
+    await envelopeDave.claim(this.signers.dave.address);
+    console.log("claimed for Dave");
+
+    console.log("trying to claim for Alice...");
+    expect(envelopeBob.claim(this.signers.alice.address)).to.be.revertedWithoutReason();
   });
 });
